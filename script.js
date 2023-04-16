b2ec2a126f97966efcaa1bedd1c3b5a0e50fda9e
sriharshamvs <sriharshamvs@gmail.com>
2023-04-16 13:51:59 -0400

    feat: added charts & fix: img scaling

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Active navigation pop 
 document.addEventListener("DOMContentLoaded", function () {
   var currentUrl = location.pathname.split("/").pop();
 
@@ -34,6 +35,7 @@ document.getElementById("bmiForm").addEventListener("submit", function (event) {
   ).innerHTML = `<strong>Your BMI is: </strong><span class="highlighted-bmi">${bmi}</span>`;
 });
 
+// Featurettes image animation
 document.addEventListener("DOMContentLoaded", function () {
   const steps = document.querySelectorAll(".step");
 
@@ -48,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Carousel animation
 document.addEventListener("DOMContentLoaded", function () {
   const quoteSlides = document.querySelectorAll(".quote-slide");
   let activeSlideIndex = 0;
@@ -59,4 +62,128 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   setInterval(changeQuote, 5000); // Change quote every 5000 milliseconds (5 seconds)
+});
+
+// Overall Fitness vs. Obesity Chart
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("fitnessData").getContext("2d");
+
+  const fitnessData = {
+    labels: ["Fitness", "Obese"],
+    datasets: [
+      {
+        label: "Population Percentage",
+        data: [35, 40],
+        backgroundColor: ["rgba(54, 162, 235, 0.2)", "rgba(255, 99, 132, 0.2)"],
+        borderColor: ["rgba(54, 162, 235, 1)", "rgba(255, 99, 132, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chart = new Chart(ctx, {
+    type: "bar",
+    data: fitnessData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+
+// Average Exercise Time (Hours/Week) Chart
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("exerciseTimeChart").getContext("2d");
+
+  const exerciseTimeData = {
+    labels: ["1990", "2000", "2010", "2020"],
+    datasets: [
+      {
+        label: "Average Time Spent Exercising (hours/week)",
+        data: [2.1, 2.6, 3.0, 3.3],
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: exerciseTimeData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+
+// Percentage of Physically Active Adults Chart
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("activeAdultsChart").getContext("2d");
+
+  const activeAdultsData = {
+    labels: ["1990", "2000", "2010", "2020"],
+    datasets: [
+      {
+        label: "Percentage of Physically Active Adults",
+        data: [45, 50, 55, 60],
+        backgroundColor: "rgba(255, 206, 86, 0.2)",
+        borderColor: "rgba(255, 206, 86, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: activeAdultsData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+
+// Nutrition Chart
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("nutritionChart").getContext("2d");
+
+  const nutritionData = {
+    labels: ["Carbohydrates", "Proteins", "Fats"],
+    datasets: [
+      {
+        label: "Percentage of Daily Recommended Intake",
+        data: [50, 30, 20],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chart = new Chart(ctx, {
+    type: "doughnut",
+    data: nutritionData,
+    options: {
+      cutoutPercentage: 60,
+    },
+  });
 });
